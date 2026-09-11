34abee5a260a78c4f1c9ea1678caddff180cf3c5
Vanessa Victorino <99343280+grauconejo13@users.noreply.github.com>
2026-09-11 17:11:55 -0500

    feat: parse auth cookies

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
@@ -18,6 +19,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: "16kb" }));
+app.use(cookieParser());
 
 app.use("/api/v1", apiRouter);
 
