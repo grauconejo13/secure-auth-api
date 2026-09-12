ad7c49e9a8aa5f5da3940dc9bb22b91b3b830b7a
Vanessa Victorino <99343280+grauconejo13@users.noreply.github.com>
2026-09-12 11:28:30 -0500

    feat: expose API documentation endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { adminRouter } from "./admin.routes.js";
 import { authRouter } from "./auth.routes.js";
+import { docsRouter } from "./docs.routes.js";
 import { healthRouter } from "./health.routes.js";
 import { userRouter } from "./user.routes.js";
 
 export const apiRouter = Router();
 
+apiRouter.use("/", docsRouter);
 apiRouter.use("/admin", adminRouter);
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/health", healthRouter);
